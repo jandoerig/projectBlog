18d3544f48a75fea7b4cf5fba7d544723fd2ea83
jandoerig <jan.doerig@bluewin.ch>
2018-11-18 20:04:42 +0100

    changes

--- project-blog/server.js
+++ project-blog/server.js
@@ -5,8 +5,12 @@ const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/blog', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'blog.html'));
 });
 
 app.listen(port, () => {
--- project-blog/src/components/homebuttonComponent/HomebuttonComponent.js
+++ project-blog/src/components/homebuttonComponent/HomebuttonComponent.js
@@ -1,10 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>$Title$</title>
-</head>
-<body>
-$END$
-</body>
-</html>
+import React, { Component } from 'react';
+import './HomebuttonComponent.css';
+
+class HomebuttonComponent extends Component {
+    render() {
+        return (
+            <div class="Homebutton">
+                <button class="Homebutton-blogButton">BLOG</button>
+                <button className="Homebutton-galleryButton">Gallery</button>
+                <button class="Homebutton-aboutMeButton">AboutMe</button>
+                <button class="Homebutton-contactMeButton">ContactMe</button>
+            </div>
+        );
+    }
+}
+
+export default HomebuttonComponent;
